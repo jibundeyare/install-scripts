d6fd5d7264fb1e1ea00308c16d0e0086210e5a81
Jibundeyare <contact@jibundeyare.com>
2020-06-12 16:21:45 +0200

    Improve documentation of mkwebsite.sh
    The documentation was using the word LOCAL_DOMAIN though the domain could
    have been a global domain name.
    The word has been repaced by DOMAIN, which can reference local domain as
    well as global domain name.
    
    The documentation did not mention the existence or the use of the
    "template-subdir.sh" or "template-subdir-symfony.sh".
    Now the documentation explains the use of these templates and give a
    examples of commands to use them.
    
    When using the "template-subdir*.sh" templates, the DOMAIN parameter is
    isgnored.
    This is information is now expicit for the user as the confirmation
    screen displays "<ignored>" for that parameter.

--- mkwebsite.sh
+++ mkwebsite.sh
@@ -3,7 +3,7 @@
 function usage {
 	this=$(basename $0)
 	cat <<-EOT
-	Usage: $this [USERNAME] [PROJECTS_DIRECTORY] [VHOST_DIRECTORY] [LOCAL_DOMAIN] [VHOST_TEMPLATE]
+	Usage: $this [USERNAME] [PROJECTS_DIRECTORY] [VHOST_DIRECTORY] [DOMAIN] [VHOST_TEMPLATE]
 
 	This script configures a new empty website.
 	It creates a new directory in your projects directory, configures the Apache2 vhost and the PHP-FPM pool files and finaly creates a default PHP home page.
@@ -17,30 +17,79 @@ function usage {
 	USERNAME should be your username.
 	PROJECTS_DIRECTORY is the directory in which you'll store all your projects.
 	VHOST_DIRECTORY is the directory in which a particular project will be stored.
-	LOCAL_DOMAIN is the domain name you will be using in your web browser to access a particular project.
+	DOMAIN is the domain name you will be using in your web browser to access a particular project.
 	VHOST_TEMPLATE is an optional parameter that specifies the Apache2 vhost template you want to use for a particular project.
-	  Possible values are: template-vhost.conf, template-vhost-symfony.conf
+	  Possible values are: template-vhost.conf, template-vhost-symfony.conf, template-subdir.conf, template-subdir-symfony.conf
+	  template-vhost*.conf files will create a new vhost.
+	  Creating a new vhost is useful when you have a domain name and you want to separate your applications into different directories and memory space.
+	  template-subdir*.conf files will create a new sub-directory.
+	  Creating a new sub-directory is useful when you do not have a domain name but still want to separate applications into different directories and memory space.
 	  Default value is "template-vhost.conf".
+
+	WARNING: when using the templates "template-subdir.conf" or "template-subdir-symfony.conf", the DOMAIN parameter will be ignored, so any value is valid.
 
 	Example 1: $this johndoe projects foo foo.local
 
 	This command will:
 
+	- make the website accessible from the url "http://foo.local"
 	- use the default "template-vhost.conf" for the VHOST_TEMPLATE value
 	- create the project directory "/home/johndoe/projects/foo"
 	- create a default PHP home page in the "/home/johndoe/projects/foo" directory
 	- create the Apache2 vhost file "/etc/apache2/sites-available/foo.conf"
 	- create the PHP-FPM pool file "/etc/php/7.4/fpm/pool.d/foo.conf"
 
-	Example 2: $this johndoe projects example example.local template-vhost-symfony.conf
+	Example 2: $this johndoe projects foo foo.example.com
 
 	This command will:
 
+	- the same things as example 1
+	- but make the website accessible from the url "http://foo.example.com"
+
+	Example 3: $this johndoe projects foo foo.local template-vhost-symfony.conf
+
+	This command will:
+
+	- make the website accessible from the url "http://foo.local"
+	- use the template "template-vhost-symfony.conf" for the VHOST_TEMPLATE value
 	- create the project directory "/home/johndoe/projects/example"
 	- create the document root directory "/home/johndoe/projects/example/public"
 	- create a default PHP home page in the "/home/johndoe/projects/example/public" directory
 	- create the Apache2 vhost file "/etc/apache2/sites-available/example.conf"
 	- create the PHP-FPM pool file "/etc/php/7.4/fpm/pool.d/example.conf"
+
+	Example 4: $this johndoe projects foo foo.example.com template-vhost-symfony.conf
+
+	This command will:
+
+	- the same things as example 3
+	- but make the website accessible from the url "http://foo.example.com"
+
+	Example 5: $this johndoe projects foo foo.local template-subdir.conf
+
+	This command will:
+
+	- ignore the domain "foo.local" parameter
+	- make the website accessible from the url "http://localhost/foo", "http://example.com/foo" or "http://1.2.3.4/foo" depending on wether you are on a local machine, a vps and if you have a domain name or not.
+	- use the template "template-subdir.conf" for the VHOST_TEMPLATE value
+	- create the project directory "/home/johndoe/projects/foo"
+	- create a default PHP home page in the "/home/johndoe/projects/foo" directory
+	- create the Apache2 conf file "/etc/apache2/sites-available/foo.conf"
+	- create the PHP-FPM pool file "/etc/php/7.4/fpm/pool.d/foo.conf"
+
+	Example 6: $this johndoe projects foo foo.local template-subdir-symfony.conf
+
+	This command will:
+
+	- ignore the domain "foo.local" parameter
+	- make the website accessible from the url "http://localhost/foo", "http://example.com/foo" or "http://1.2.3.4/foo" depending on wether you are on a local machine, a vps and if you have a domain name or not.
+	- use the template "template-subdir-symfony.conf" for the VHOST_TEMPLATE value
+	- create the project directory "/home/johndoe/projects/example"
+	- create the document root directory "/home/johndoe/projects/example/public"
+	- create a default PHP home page in the "/home/johndoe/projects/example/public" directory
+	- create the Apache2 conf file "/etc/apache2/sites-available/example.conf"
+	- create the PHP-FPM pool file "/etc/php/7.4/fpm/pool.d/example.conf"
+
 	EOT
 }
 
@@ -52,7 +101,7 @@ else
 	username="$1"
 	projects_directory="$2"
 	vhost_directory="$3"
-	local_domain="$4"
+	domain="$4"
 
 	if [ $# -gt 4 ]; then
 		vhost_template="$5"
@@ -65,11 +114,15 @@ else
 		vhost_template="template-vhost.conf"
 	fi
 
+	if [ "$vhost_template" == "template-subdir.conf" ] || [ "$vhost_template" == "template-subdir-symfony.conf" ]; then
+		domain="<ignored>"
+	fi
+
 	cat <<-EOT
 	USERNAME: $username
 	PROJECTS_DIRECTORY: $projects_directory
 	VHOST_DIRECTORY: $vhost_directory
-	LOCAL_DOMAIN: $local_domain
+	DOMAIN: $domain
 	VHOST_TEMPLATE: $vhost_template
 
 	EOT
@@ -132,7 +185,7 @@ sudo cp $vhost_template /etc/apache2/sites-available/$vhost_directory.conf
 sudo sed -i "s/{username}/$username/g" /etc/apache2/sites-available/$vhost_directory.conf
 sudo sed -i "s/{projects_directory}/$projects_directory/g" /etc/apache2/sites-available/$vhost_directory.conf
 sudo sed -i "s/{vhost_directory}/$vhost_directory/g" /etc/apache2/sites-available/$vhost_directory.conf
-sudo sed -i "s/{local_domain}/$local_domain/g" /etc/apache2/sites-available/$vhost_directory.conf
+sudo sed -i "s/{domain}/$domain/g" /etc/apache2/sites-available/$vhost_directory.conf
 
 # enable vhost
 sudo a2ensite $vhost_directory.conf
