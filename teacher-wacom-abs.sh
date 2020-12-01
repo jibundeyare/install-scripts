0b29424cd06dd53898dd613e38e1993851e19ad2
Jibundeyare <contact@jibundeyare.com>
2020-12-01 13:39:30 +0100

    Add scripts to configure wacom tablets
    These script can make the pointer position relative or absolute.

---
+++ teacher-wacom-abs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# set wacom tablet to relative mode
+xsetwacom --set 13 Mode Absolute
+xsetwacom --set 14 Mode Absolute
+
---
+++ teacher-wacom-rel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# set wacom tablet to relative mode
+xsetwacom --set 13 Mode Relative
+xsetwacom --set 14 Mode Relative
+
