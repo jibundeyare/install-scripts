bf386ba9ea325774aa3c0a7dfaa234cf2bbda84b
Jibundeyare <contact@jibundeyare.com>
2020-12-01 13:59:59 +0100

    Make the teacher wacom conf customisable
    The device ids of the wacom tablet was hard coded in the scripts.
    
    Now a external conf file makes it possible to customize the
    configuration without modifying the scripts.

--- teacher-wacom-rel.sh
+++ teacher-wacom-rel.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-# set wacom tablet to relative mode
-xsetwacom --set 13 Mode Relative
-xsetwacom --set 14 Mode Relative
+teacher_wacom_conf_file="teacher-wacom-conf.sh"
+
+if [ ! -f "$teacher_wacom_conf_file" ]; then
+	echo "error: missing teacher wacom conf file '$teacher_wacom_conf_file'"
+	echo ""
+	echo "did you forget to create one ?"
+	echo "use '$teacher_wacom_conf_file.dist' as a base file"
+	exit 1
+fi
+
+source $teacher_wacom_conf_file
+
+for wacom_device in $wacom_devices; do
+	# set wacom tablet to relative mode
+	xsetwacom --set $wacom_device Mode Relative
+done
 
