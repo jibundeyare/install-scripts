d907bb2419e6199ab04f829bf3af1b40a0db2b66
jibundeyare <contact@jibundeyare.com>
2021-01-12 19:37:02 +0100

    Add modules to php
    Add the following modules to php:
    
    - php7.4-gd
    - php7.4-soap
    - php7.4-xmlrpc

--- install-amp.sh
+++ install-amp.sh
@@ -72,7 +72,7 @@ sudo apt install -y apache2
 sudo apt install -y mariadb-client mariadb-server
 
 # install php7.4
-sudo apt install -y imagemagick libapache2-mod-php7.4 php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-imagick php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-phpdbg php7.4-readline php7.4-xml php7.4-zip
+sudo apt install -y imagemagick libapache2-mod-php7.4 php7.4 php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-gd php7.4-imagick php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-phpdbg php7.4-readline php7.4-soap php7.4-xml php7.4-xmlrpc php7.4-zip
 
 # configure php7.4
 
