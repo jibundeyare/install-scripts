1ea57d1eac68b7f687ed20b337332973574a3a81
jibundeyare <contact@jibundeyare.com>
2022-08-25 21:07:06 +0200

    Fix upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -5,5 +5,6 @@ default_vhost_directory="www"
 sudo sed -i "s/#ServerName/ServerName/g" /etc/apache2/sites-available/000-default.conf
 sudo mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$default_vhost_directory.conf
 sudo mv /etc/apache2/sites-available/000-default.conf.orig /etc/apache2/sites-available/000-default.conf
+sudo rm /etc/apache2/sites-enabled/000-default.conf
 sudo ln -s ../sites-available/$default_vhost_directory.conf /etc/apache2/sites-enabled/000-default.conf
 
